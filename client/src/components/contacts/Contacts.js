cc4a620ab15e54e356113ac9a8abe92202f1a0ce
Deleon Francis <dxfrancis.coding@gmail.com>
2021-04-19 11:11:13 -0400

    filter UI now works.

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -6,15 +6,23 @@ function Contacts() {
   // gets access to any state or actions associated with ContactState
   const contactContext = useContext(ContactContext);
 
-  const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
+
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
 
   return (
     <Fragment>
-      {contacts.map(contact => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      {filtered !== null
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))}
     </Fragment>
   );
 }
- 
+
 export default Contacts;
